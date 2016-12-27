d87626d5bd5631f63ae4120a312b1b24729cfe89
Harry Merzin <hmerzin@gmail.com>
2016-12-27 10:24:12 -0500

    add delete feature (need to delete data before deleting row in tableview)

--- learning coredata/NotesVC.swift
+++ learning coredata/NotesVC.swift
@@ -45,11 +45,42 @@ class NotesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         for note in notesArray {
             print(note.text)
         }
+        tableView.reloadData()
+    }
+    
+    func delete(object: Note, indexPath: IndexPath) {
+        print("count: \(notesArray.count)")
+        do {
+            try context.delete(object)
+        } catch {
+            fatalError("couldn't delete note")
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            fatalError("couldn't save")
+        }
+        notesArray.remove(at: indexPath.row)
+        //tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("numofrowsinsec: \(notesArray.count)")
         return notesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        delete(object: notesArray[indexPath.row], indexPath: indexPath)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -58,7 +89,6 @@ class NotesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         cell.label.text = notesArray[indexPath.row].text
         print("text: \(cell.label.text)")
         return cell
-        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
